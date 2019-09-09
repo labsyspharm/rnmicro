a86cf85aa71309f1e46d980b33d76003a3590f24
bioinfonerd <johnsonnathant@gmail.com>
2019-09-09 12:31:09 -0400

    updated bash clean script to current folder structure

--- bin/clean.sh
+++ bin/clean.sh
@@ -8,9 +8,9 @@ echo 'Remove folder structure except raw images'
 rm -r */cell_states  
 rm -r */clustering  
 rm -r */dearray  
-rm -r feature_extraction  
-rm -r illumination_profiles  
-rm -r prob_maps  
-rm -r registration
-rm -r segmentation
+rm -r */feature_extraction  
+rm -r */illumination_profiles  
+rm -r */prob_maps  
+rm -r */registration
+rm -r */segmentation
 echo 'Cleaning Completed'
