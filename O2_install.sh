e046a83987419fe73ad86bb57c2c044d043dc4ed
bioinfonerd <johnsonnathant@gmail.com>
2019-09-09 11:44:33 -0400

    add test environment to path

--- O2_install.sh
+++ O2_install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 #attach pipeline to bash path
-echo 'CYCIF=/n/groups/lsp/cycif/CyCif_Manager/O2:/n/groups/lsp/cycif/CyCif_Manager/bin' >> ~/.bash_profile
+#echo 'CYCIF=/n/groups/lsp/cycif/CyCif_Manager/O2:/n/groups/lsp/cycif/CyCif_Manager/bin' >> ~/.bash_profile
+#echo 'export PATH=$CYCIF:$PATH' >> ~/.bash_profile
+
+#attach test pipeline space to bash path
+echo 'CYCIF=/n/groups/lsp/cycif/cycif_pipeline_testing_space/mcmicro/O2:/n/groups/lsp/cycif/cycif_pipeline_testing_space/mcmicro/bin' >> ~/.bash_profile
 echo 'export PATH=$CYCIF:$PATH' >> ~/.bash_profile
 
 #reload current environment 
