67572337d3c52e4869b2d356a1e764b7ab03f975
bioinfonerd <johnsonnathant@gmail.com>
2019-09-10 15:08:44 -0400

    Update to V1.1: Parallelization

--- O2_install.sh
+++ O2_install.sh
@@ -5,7 +5,7 @@
 #echo 'export PATH=$CYCIF:$PATH' >> ~/.bash_profile
 
 #attach test pipeline space to bash path
-echo 'CYCIF=/n/groups/lsp/cycif/cycif_pipeline_testing_space/mcmicro/O2:/n/groups/lsp/cycif/cycif_pipeline_testing_space/mcmicro/bin' >> ~/.bash_profile
+echo 'CYCIF=/n/groups/lsp/cycif/mcmicro/O2:/n/groups/lsp/cycif/mcmicro/bin' >> ~/.bash_profile
 echo 'export PATH=$CYCIF:$PATH' >> ~/.bash_profile
 
 #reload current environment 
