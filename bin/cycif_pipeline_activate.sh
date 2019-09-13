eb01be410eaebf661d92861dc82bdf9ce2458726
bioinfonerd <johnsonnathant@gmail.com>
2019-09-13 14:35:26 -0400

    Update to V1.2

--- bin/cycif_pipeline_activate.sh
+++ bin/cycif_pipeline_activate.sh
@@ -1,4 +1,4 @@
 module load conda2/4.2.13
 source activate /n/groups/lsp/cycif/cycif_pipeline_testing_space/mcmicro/environments/cycif_pipeline/
-python /n/groups/lsp/cycif/cycif_pipeline_testing_space/mcmicro/O2/CyCif_Pipeline_O2_v1.1.py $1
+python /n/groups/lsp/cycif/cycif_pipeline_testing_space/mcmicro/O2/CyCif_Pipeline_O2_v1.2.py $1
 conda deactivate
