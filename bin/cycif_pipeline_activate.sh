7db41b3280dc0663656a81e2842a0d97e07763be
bioinfonerd <johnsonnathant@gmail.com>
2019-10-09 16:38:08 -0400

    fix argument pass bug

--- bin/cycif_pipeline_activate.sh
+++ bin/cycif_pipeline_activate.sh
@@ -1,5 +1,5 @@
 module load conda2/4.2.13
 source activate /n/groups/lsp/cycif/cycif_pipeline_testing_space/mcmicro/environments/cycif_pipeline/
 #run program, echo to screen and save to output file
-python /n/groups/lsp/cycif/cycif_pipeline_testing_space/mcmicro/O2/CyCif_Pipeline_O2_v1.2.py $1 | tee CyCif_Pipeline_log.txt
+python /n/groups/lsp/cycif/cycif_pipeline_testing_space/mcmicro/O2/CyCif_Pipeline_O2_v1.2.py $1 $2 $3 | tee CyCif_Pipeline_log.txt
 conda deactivate
