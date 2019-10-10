685b072b33fe75c175506f3b166e8f30205fac08
bioinfonerd <johnsonnathant@gmail.com>
2019-10-10 14:38:58 -0400

    improve Rename program for project use

--- bin/Rename.py
+++ bin/Rename.py
@@ -1,29 +1,26 @@
 #Created: Nathan T. Johnson
 #Purpose: rename files so that order stamp is based on date for ashlar input and correct stitching
 #call program on project folder parent
+#input: project path
+#output: raw image file renamed to remove what the user modified
 
 
 #libraries
 import os
 import glob
 
-#not needed if run on command line
-#os.chdir('/home/bionerd/test_dir')
+#master_dir = os.path.normpath(sys.argv[1])
+master_dir = os.path.normpath('/home/bionerd/Dana_Farber/CyCif/git/mcmicro/example_data/')
 
 #rename all files using order
-files = os.listdir('.')
-#only needed if certain files should not be processed
-files.remove('Rename.py')
+files = next(os.walk(master_dir))[1]
 
 #rename files to ensure correct order
 for i in iter(files):
 	print('Processing File:'+i)
-	to_process = glob.glob(''.join([i + '/raw_files/*']))
+	to_process = glob.glob(''.join([master_dir + '/' + i + '/raw_files/*']))
 	for n in to_process:        
 		rename = n.split('/')[-1].split('Scan_')[-1]
-		tmp = n.split('/')[:-1]
-		tmp.append(rename)
-		rename = '/'.join(tmp)
-		output = ''.join(['mv ' + n + ' ' + rename]) #execution 
-		#print(output)       
+		rename = master_dir + '/' + rename
+		output = ''.join(['mv ' + n + ' ' + rename]) #execution
 		os.system(output)
