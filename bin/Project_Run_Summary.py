d3d6fcf002e8201603629fc0372dde5e1bf2e400
bioinfonerd <johnsonnathant@gmail.com>
2019-10-25 10:51:23 -0400

    debugging why dataframes arenot merging

--- bin/Project_Run_Summary.py
+++ bin/Project_Run_Summary.py
@@ -57,92 +57,92 @@ samples = next(os.walk(master_dir))[1]
 
 #process summary per sample
 for i in samples:
-print('Processing ' + i)
+    print('Processing ' + i)
 
-# Project Name
-Project = master_dir.split('/')[-1]
+    # Project Name
+    Project = master_dir.split('/')[-1]
 
-# Sample Name
-Sample = i
+    # Sample Name
+    Sample = i
 
-#CyCle Number
-if os.access(''.join([master_dir + '/' + i + '/raw_files']),mode=0):
-    #Find the number of Cycles
-    Cycle_Number = len(glob.glob(''.join([master_dir + '/' + i + '/raw_files/*' + microscope_check(i,master_dir)])))
-else:
-    Cycle_Number = 'Fail'
+    #CyCle Number
+    if os.access(''.join([master_dir + '/' + i + '/raw_files']),mode=0):
+        #Find the number of Cycles
+        Cycle_Number = len(glob.glob(''.join([master_dir + '/' + i + '/raw_files/*' + microscope_check(i,master_dir)])))
+    else:
+        Cycle_Number = 'Fail'
 
-# Illumination Progress (dfp.tif and ffp)
-if os.access(''.join([master_dir + '/' + i + '/illumination_profiles']),mode=0):
+    # Illumination Progress (dfp.tif and ffp)
+    if os.access(''.join([master_dir + '/' + i + '/illumination_profiles']),mode=0):
 
-    #if number of cycles matches the number of expected illumination profiles then remove illumination from pipeline
-    if (len(glob.glob(''.join([master_dir + '/' + i + '/illumination_profiles/*-dfp.tif']))) == Cycle_Number) & (len(glob.glob(''.join([master_dir + '/' + i + '/illumination_profiles/*-ffp.tif']))) == Cycle_Number):
-        Illumination = 'Pass'
+        #if number of cycles matches the number of expected illumination profiles then remove illumination from pipeline
+        if (len(glob.glob(''.join([master_dir + '/' + i + '/illumination_profiles/*-dfp.tif']))) == Cycle_Number) & (len(glob.glob(''.join([master_dir + '/' + i + '/illumination_profiles/*-ffp.tif']))) == Cycle_Number):
+            Illumination = 'Pass'
+        else:
+            Illumination = 'Fail'
     else:
         Illumination = 'Fail'
-else:
-    Illumination = 'Fail'
 
-# Stitcher Progress (*ome.tif)
-if os.access(''.join([master_dir + '/' + i + '/registration']), mode=0):
-    if (len(glob.glob(''.join([master_dir + '/' + i + '/registration/*.ome.tif']))) == 1):
-        Stitcher = 'Pass'
+    # Stitcher Progress (*ome.tif)
+    if os.access(''.join([master_dir + '/' + i + '/registration']), mode=0):
+        if (len(glob.glob(''.join([master_dir + '/' + i + '/registration/*.ome.tif']))) == 1):
+            Stitcher = 'Pass'
+        else:
+            Stitcher = 'Fail'
     else:
         Stitcher = 'Fail'
-else:
-    Stitcher = 'Fail'
 
-# Probability Mapper (*ContoursPM_1.tif *NucleiPM_1.tif)
-if os.access(''.join([master_dir + '/' + i + '/prob_maps']), mode=0):
-    if (len(glob.glob(''.join([master_dir + '/' + i + '/prob_maps/*ContoursPM_1.tif']))) == 1) & (len(glob.glob(''.join([master_dir + '/' + i + '/prob_maps/*NucleiPM_1.tif']))) == 1):
-        Prob_Mapper = 'Pass'
+    # Probability Mapper (*ContoursPM_1.tif *NucleiPM_1.tif)
+    if os.access(''.join([master_dir + '/' + i + '/prob_maps']), mode=0):
+        if (len(glob.glob(''.join([master_dir + '/' + i + '/prob_maps/*ContoursPM_1.tif']))) == 1) & (len(glob.glob(''.join([master_dir + '/' + i + '/prob_maps/*NucleiPM_1.tif']))) == 1):
+            Prob_Mapper = 'Pass'
+        else:
+            Prob_Mapper = 'Fail'
     else:
         Prob_Mapper = 'Fail'
-else:
-    Prob_Mapper = 'Fail'
 
-# Segmentation (cellMask.tif cellOutlines.tif nucleiMask.tif nucleiOutlines.tif segParams.mat)
-if os.access(''.join([master_dir + '/' + i + '/segmentation']), mode=0):
-    if (len(glob.glob(''.join([master_dir + '/' + i + '/segmentation/*.tif']))) == 4):
-        Segmentation = 'Pass'
+    # Segmentation (cellMask.tif cellOutlines.tif nucleiMask.tif nucleiOutlines.tif segParams.mat)
+    if os.access(''.join([master_dir + '/' + i + '/segmentation']), mode=0):
+        if (len(glob.glob(''.join([master_dir + '/' + i + '/segmentation/*.tif']))) == 4):
+            Segmentation = 'Pass'
+        else:
+            Segmentation = 'Fail'
     else:
         Segmentation = 'Fail'
-else:
-    Segmentation = 'Fail'
 
-# Feature Extraction (if feature_extractor ran image_1 / feature_extraction = Cell_image_1*.mat == length of markers.csv & sample_name.csv)
-if os.access(''.join([master_dir + '/' + i + '/feature_extraction']), mode=0):
-    if os.access(''.join([master_dir + '/markers.csv']), mode=0):
-        markers = pd.read_csv(''.join([master_dir + '/markers.csv']))
+    # Feature Extraction (if feature_extractor ran image_1 / feature_extraction = Cell_image_1*.mat == length of markers.csv & sample_name.csv)
+    if os.access(''.join([master_dir + '/' + i + '/feature_extraction']), mode=0):
+        if os.access(''.join([master_dir + '/markers.csv']), mode=0):
+            markers = pd.read_csv(''.join([master_dir + '/markers.csv']))
 
-        # if files exist, remove feature extracto from pipeline
-        if (len(glob.glob(''.join([master_dir + '/' + i + '/feature_extraction/Cell*.mat']))) == len(markers)+1):
-            if os.access(''.join([master_dir + '/' + i + '.csv']), mode=0):
-                Feature_Extraction = 'Pass'
+            # if files exist, remove feature extracto from pipeline
+            if (len(glob.glob(''.join([master_dir + '/' + i + '/feature_extraction/Cell*.mat']))) == len(markers)+1):
+                if os.access(''.join([master_dir + '/' + i + '.csv']), mode=0):
+                    Feature_Extraction = 'Pass'
+                else:
+                    Feature_Extraction = 'Fail: Final csv file not found'
             else:
-                Feature_Extraction = 'Fail: Final csv file not found'
+                Feature_Extraction = 'Fail: Not all channels extracted'
         else:
-            Feature_Extraction = 'Fail: Not all channels extracted'
+            Feature_Extraction = 'Fail: markers.csv file not found'
     else:
-        Feature_Extraction = 'Fail: markers.csv file not found'
-else:
-    Feature_Extraction = 'Fail: Folder not found'
+        Feature_Extraction = 'Fail: Folder not found'
 
-#saving results for per image
-if i == samples[0]:
-    df = pd.DataFrame({'Project': [Project],'Sample': [Sample],'Number of Cycles': [Cycle_Number],
-                       'Illumination': [Illumination],'Stitcher': [Stitcher],'Probability Maps': [Prob_Mapper],
-                      'Segmentation': [Segmentation],'Feature Extraction': [Feature_Extraction]})
-    output=df
-    print(i)
-    print(output)
-else:
-    print(i)
-    print(df)
-    df = pd.DataFrame({'Project': [Project],'Sample': [Sample],'Number of Cycles': [Cycle_Number],
-                       'Illumination': [Illumination],'Stitcher': [Stitcher],'Probability Maps': [Prob_Mapper],
-                      'Segmentation': [Segmentation],'Feature Extraction': [Feature_Extraction]})
-    output = pd.merge(df,output,how='outer')
+    #saving results for per image
+    if i == samples[0]:
+        df = pd.DataFrame({'Project': [Project],'Sample': [Sample],'Number of Cycles': [Cycle_Number],
+                           'Illumination': [Illumination],'Stitcher': [Stitcher],'Probability Maps': [Prob_Mapper],
+                          'Segmentation': [Segmentation],'Feature Extraction': [Feature_Extraction]})
+        output=df
+        print(i)
+        print(output)
+    else:
+        print(i)
+        print(df)
+        df = pd.DataFrame({'Project': [Project],'Sample': [Sample],'Number of Cycles': [Cycle_Number],
+                           'Illumination': [Illumination],'Stitcher': [Stitcher],'Probability Maps': [Prob_Mapper],
+                          'Segmentation': [Segmentation],'Feature Extraction': [Feature_Extraction]})
+        output = pd.merge(df,output,how='outer')
 
 #save results
 output.to_csv('CyCif_Run_Summary.csv',index=False)
