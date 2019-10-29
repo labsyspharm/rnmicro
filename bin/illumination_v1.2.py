8231dcd82c135280a2f610d628fc5e883b66714d
bioinfonerd <johnsonnathant@gmail.com>
2019-10-29 15:06:21 -0400

    forgot to comment out local paths

--- bin/illumination_v1.2.py
+++ bin/illumination_v1.2.py
@@ -42,7 +42,7 @@ def microscope_check(current_sample):
 #sys.argv.append(os.path.normpath('/home/bionerd/Dropbox/@Dana Farber/CyCif/git/mcmicro/example_data/image_1')) #local testing
 path_exp = pathlib.Path('/'.join([str(sys.argv[1])]))
 #dictionary of parameters
-sys.argv.append(str({'lambda_flat': 0.1, 'lambda_dark': 0.01, 'estimate_flat_field_only': False, 'max_number_of_fields_used': 'None'})) #local testing
+#sys.argv.append(str({'lambda_flat': 0.1, 'lambda_dark': 0.01, 'estimate_flat_field_only': False, 'max_number_of_fields_used': 'None'})) #local testing
 #passing a dictionary from command line
 parameters = eval(sys.argv[2])
 
