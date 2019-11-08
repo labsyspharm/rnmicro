9a2789c7825149f54ee2c325b3defc5e29d42270
bioinfonerd <johnsonnathant@gmail.com>
2019-11-08 15:45:17 -0500

    update to jeremy original script

--- bin/mcmicro-download-exemplar-001.sh
+++ bin/mcmicro-download-exemplar-001.sh
@@ -6,7 +6,7 @@ base_url='https://mcmicro.s3.amazonaws.com/exemplars/001/exemplar-001'
 
 # We will use this name for both the top level project directory as well as the
 # single dataset directory inside it.
-dataset="exemplar_001"
+dataset="exemplar-001"
 
 echo "Creating project directory '$dataset'"
 mkdir -p "$dataset"
@@ -21,13 +21,13 @@ echo
 
 mkdir -p "$dataset"
 cd "$dataset"
-mkdir -p raw_files illumination_profiles
+mkdir -p raw_images illumination_profiles
 base=$(pwd)
 for i in `seq 10`; do
   name="exemplar-001-cycle-$(printf %02d $i)"
-  cd "$base/raw_files"
+  cd "$base/raw_images"
   echo "$name raw image"
-  curl -f -# -O "$base_url/raw_files/$name.ome.tiff"
+  curl -f -# -O "$base_url/raw_images/$name.ome.tiff"
   cd "$base/illumination_profiles"
   echo "$name illumination profiles (2)"
   curl -f -# -O "$base_url/illumination_profiles/$name-dfp.tif"
